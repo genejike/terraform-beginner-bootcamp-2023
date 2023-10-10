1a55b8cf5c285cde88619cd75b374ba04d758af6
Ejike Mary-cynthia <genejike2014@gmail.com>
2023-10-10 18:22:47 +0000

    create a simple S3 Bucket  #13

--- main.tf
+++ main.tf
@@ -4,7 +4,16 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.20.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
+
 }
 
 provider "random" {
@@ -12,12 +21,15 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = true
-  override_special = ""
+  lower = true
+  upper = false
+  length = 32
+  special = false
 }
 
-
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+}
 output "random_bucket_name"{
     value = random_string.bucket_name.result
 
