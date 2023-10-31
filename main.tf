f7b88682faabedfbb874b044597c48defc5caa20
Ejike Mary-cynthia <genejike2014@gmail.com>
2023-10-31 09:43:23 +0000

    #38 upload index.html if a content version has changed.

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ terraform {
 }
 
 module "terrahouse_aws" {
+
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  content_version = var.content_version
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   }
