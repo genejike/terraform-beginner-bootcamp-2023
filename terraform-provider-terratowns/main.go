4c0c804f717a11cfd346c6263feef57ed50d253c
Ejike Mary-cynthia <genejike2014@gmail.com>
2023-11-22 09:15:23 +0000

    #46 resource skeleton

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -8,8 +8,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-
-	"github.com/google/uuid"
+    "github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
